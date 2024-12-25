5a0dafcf224573a68c98b868911ca16b58ad92a3
Anirudh <95073991+ani-2008@users.noreply.github.com>
2024-12-25 06:55:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import random    
 def game():
-    print("Anirudh's food name guess\nYou have 8 guesses\nIf you get 10 points you win\nGamestarts")
+    print("Anirudh's food name guess\nYou have 10 guesses\nIf you get 10 points you win\nGamestarts")
     point = 0
     i = 0
     foods = ["apple","banana","burger","cheese","grapes","pizza","pasta","spaghetti","burrito","taco",
              "french fries","hot dog","waffle","potato","okra","eggplant","nachos",
              "quesadilla","pazole","tacos al pastor","elote","tostadas","cobb salad","hamburger"]
     print(", ".join(foods))
-    while i < 8 and point <= 10 :
+    while i < 10 and point <= 10 :
         word = random.choice(foods)
         guess_word = input("Enter a guess: ")
         if guess_word.lower() == word:
@@ -18,7 +18,7 @@ def game():
             i += 1
             print("point:",point,"no of lose:",i)
             print(word)
-        if i == 8 :
+        if i == 10 :
             print("Oops you lose!")
         if point == 10:
             print("You win!!")
